882b3f63de8c0806755973a3b9c7fed9878c2f3a
PavelAltynnikov <robenzon11@gmail.com>
2023-08-26 21:29:57 +0400

    add enable and disable logic for WeaponInput

--- Assets/_Sources/Scripts/WeaponInput.cs
+++ Assets/_Sources/Scripts/WeaponInput.cs
@@ -1,14 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 public class WeaponInput : MonoBehaviour
 {
     [SerializeField] private Weapon _weapon;
 
+    public bool _canBeUsed;
+
+    public void Awake()
+    {
+        _canBeUsed = true;
+    }
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (_canBeUsed == false
+            || Input.GetMouseButtonDown(0) == false)
         {
-            _weapon.Shoot();
+            return;
         }
+
+        _weapon.Shoot();
+    }
+
+    public void Enable()
+    {
+        _canBeUsed = true;
+    }
+
+    public void Disable()
+    {
+        _canBeUsed = false;
     }
 }
