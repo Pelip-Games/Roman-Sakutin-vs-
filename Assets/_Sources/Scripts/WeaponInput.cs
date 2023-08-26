0d1dbe0b3af3174b9a73418af844f1ed000637b9
PavelAltynnikov <robenzon11@gmail.com>
2023-08-26 17:21:50 +0400

    add weapon and bullet scripts

---
+++ Assets/_Sources/Scripts/WeaponInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class WeaponInput : MonoBehaviour
+{
+    [SerializeField] private Weapon _weapon;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _weapon.Shoot();
+        }
+    }
+}
