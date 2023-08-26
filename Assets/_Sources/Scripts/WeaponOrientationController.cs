7fff472b1c9ed28dd096833967a4af004311748e
PavelAltynnikov <robenzon11@gmail.com>
2023-08-26 16:22:26 +0400

    add weapon orientation controller class

---
+++ Assets/_Sources/Scripts/WeaponOrientationController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Sources.Scripts
+{
+    public class WeaponOrientationController : MonoBehaviour
+    {
+        [SerializeField] private Transform _aim;
+
+        private void Update()
+        {
+            transform.LookAt(_aim);
+        }
+    }
+}
