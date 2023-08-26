8d3bc57aa6b56870682bb629bd47b6230a26a4db
PavelAltynnikov <robenzon11@gmail.com>
2023-08-26 21:30:10 +0400

    add SceneContext class

---
+++ Assets/_Sources/Scripts/SceneContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class SceneContext : MonoBehaviour
+{
+    [SerializeField] private Wallet _wallet;
+    [SerializeField] private GameMenu _gameMenu;
+    [SerializeField] private WeaponInput _weaponInput;
+
+    private void OnEnable()
+    {
+        _wallet.WalletChanged += UpdateUIBalance;
+
+        _wallet.WalletIsEmpty += DisableWeapon;
+    }
+
+    private void OnDisable()
+    {
+        _wallet.WalletChanged -= UpdateUIBalance;
+
+        _wallet.WalletIsEmpty -= DisableWeapon;
+    }
+
+    private void DisableWeapon(object sender, EventArgs e)
+    {
+        _weaponInput.Disable();
+    }
+
+    private void UpdateUIBalance(object sender, WalletChangedEventArgs e)
+    {
+        _gameMenu.UpdateBalance(e.Balance);
+    }
+}
