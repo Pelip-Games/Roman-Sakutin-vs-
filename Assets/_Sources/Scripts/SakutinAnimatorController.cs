99ff630ffda1bca20d87a5a061b09cb7b1b79ecd
PavelAltynnikov <robenzon11@gmail.com>
2023-08-27 13:15:22 +0400

    add SakutinAnimatorController class

---
+++ Assets/_Sources/Scripts/SakutinAnimatorController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SakutinAnimatorController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    
+    private bool _isRunning;
+
+    public void SetWeapon()
+    {
+        _animator.SetBool("isArmed", true);
+    }
+
+    public void DropWeapon()
+    {
+        _animator.SetBool("isArmed", false);
+    }
+
+    public void SetIdle()
+    {
+        if (_isRunning == false)
+        {
+            return;
+        }
+
+        _isRunning = false;
+        _animator.SetTrigger("isIdle");
+    }
+
+    public void SetRunning()
+    {
+        if (_isRunning)
+        {
+            return;
+        }
+
+        _isRunning = true;
+        _animator.SetTrigger("isRunning");
+    }
+
+    public void SetHurt()
+    {
+        _animator.SetTrigger("isHurt");
+    }
+
+    public void SetStun()
+    {
+        _animator.SetTrigger("isStunned");
+    }
+}
