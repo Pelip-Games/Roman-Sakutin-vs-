60ee62ae11336ef58f9bd95408e052138703e41f
Pelip Alexandr <sasha.pelip@gmail.com>
2023-08-27 19:13:21 +0300

    Fix panels

--- Assets/_Sources/Scripts/GameEnd/GameOverPanel.cs
+++ Assets/_Sources/Scripts/GameEnd/GameOverPanel.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class GameOverPanel : MonoBehaviour
 {
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
