4eda21ea146ec7605ebd24ef384b7ce3a4ba1f40
PavelAltynnikov <robenzon11@gmail.com>
2023-08-27 14:27:33 +0400

    add weapon sprite into weapon

--- Assets/_Sources/Scripts/Weapon.cs
+++ Assets/_Sources/Scripts/Weapon.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 internal class Weapon : MonoBehaviour
 {
     [SerializeField] private Bullet _bulletPrefab;
+    [SerializeField] private SpriteRenderer _weaponSprite;
 
     public bool IsActive { get; private set; }
 
     public void Activate()
     {
         IsActive = true;
+        _weaponSprite.gameObject.SetActive(IsActive);
     }
 
     public void Deactivate()
     {
         IsActive = false;
+        _weaponSprite.gameObject.SetActive(IsActive);
     }
 
     internal void Shoot()
