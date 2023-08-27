73a8364d06b24a560f547af6d078976a07be05bc
PavelAltynnikov <robenzon11@gmail.com>
2023-08-27 13:58:22 +0400

    add activate behavior for Weapon class

--- Assets/_Sources/Scripts/Weapon.cs
+++ Assets/_Sources/Scripts/Weapon.cs
@@ -4,6 +4,18 @@ internal class Weapon : MonoBehaviour
 {
     [SerializeField] private Bullet _bulletPrefab;
 
+    public bool IsActive { get; private set; }
+
+    public void Activate()
+    {
+        IsActive = true;
+    }
+
+    public void Deactivate()
+    {
+        IsActive = false;
+    }
+
     internal void Shoot()
     {
         Instantiate(_bulletPrefab, transform.position, transform.rotation);
